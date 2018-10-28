d0e08e5f4b47a19dffcb101184c5545ab3834129
danielblacha <daniel.blacha@gmail.com>
2018-10-28 15:22:54 +0100

    next commit

--- src/main/java/pl/akademiakodu/blogApplication/model/Post.java
+++ src/main/java/pl/akademiakodu/blogApplication/model/Post.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -31,4 +32,8 @@ public class Post {
     public Post() {
 
     }
+
+    @OneToMany
+    @JoinColumn(name = "postId")
+    List<PostComment> comments;
 }
