d0c981c47ee9a11384458265535837681ab8a09c
danielblacha <daniel.blacha@gmail.com>
2018-10-28 14:52:44 +0100

    next commit

--- src/main/java/pl/akademiakodu/blogApplication/controller/MainController.java
+++ src/main/java/pl/akademiakodu/blogApplication/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.akademiakodu.blogApplication.model.Post;
@@ -38,6 +39,12 @@ public class MainController {
     @GetMapping("/posts")
     public String postsPage(Model model) {
         model.addAttribute("posts", postRepository.findAll());
+        return "posts";
+    }
+
+    @GetMapping("/posts/{title}")
+    public String postsByTitle(@PathVariable String title, Model model) {
+        model.addAttribute("posts", postRepository.findAllByTitleContains(title));
         return "posts";
     }
 }
--- src/main/java/pl/akademiakodu/blogApplication/repository/PostRepository.java
+++ src/main/java/pl/akademiakodu/blogApplication/repository/PostRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.akademiakodu.blogApplication.model.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
+
+     List<Post> findAllByTitleContains(String title);
 }
