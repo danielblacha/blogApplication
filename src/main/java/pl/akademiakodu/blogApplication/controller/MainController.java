67139a4af4d788ceb7482ab72b31638f90eac28c
danielblacha <daniel.blacha@gmail.com>
2018-10-28 11:52:34 +0100

    next commit

--- src/main/java/pl/akademiakodu/blogApplication/controller/MainController.java
+++ src/main/java/pl/akademiakodu/blogApplication/controller/MainController.java
@@ -1,13 +1,15 @@
 package pl.akademiakodu.blogApplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("name", "Daniel");
         return "index";
     }
 }
